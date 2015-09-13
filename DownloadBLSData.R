81435766113bc3f819ca942efc27ee638b1c0e37
Wat Hughes <wat.hughes@gmail.com>
2015-09-12 21:44:58 -0400

    Hand Merge

--- DownloadBLSData.R
+++ DownloadBLSData.R
@@ -1,48 +1,66 @@
 # Some of the BLS data files are large enough that it is impractical to work with
 # them over the net. This R script downloads any that are missing from the target
 # directory.
+#
+# Note, git auto merge failed on 9/12/2015 so Wat hand merged changes from Wayne.
 
-# Modify these to fit your environment:
-DDLRoot <- '~/Downloads/bls'
-ProjectDir <- paste0(DDLRoot, '/03-the-redline')
-DataDir <- paste0(DDLRoot, '/Data')
-OrigDataDir <- paste0(DataDir, '/BLSOrig')
-BLSDataURL <- 'http://download.bls.gov/pub/time.series/cs'
+# Globals
 
-# setwd(ProjectDir)
+BLSDataURL ='http://download.bls.gov/pub/time.series/cs'
+
+hostname = system('hostname', intern=T)
+
+# Add support for your system by name by adding another else if...
+
+if (hostname == 'AJ')
+{
+    DDLRoot = 'E:/wat/misc/DDL'
+    DataDir = paste0(DDLRoot,'/Data')
+} else if (hostname == 'VM-EP-3')
+{
+    DDLRoot = 'd:/RProjects' # Oops
+    DataDir = paste0(DDLRoot,'/RedLineData')
+} else # Wayne's system, name not yet recorded here.
+{
+    DDLRoot = '~/Downloads/bls'
+    DataDir = paste0(DDLRoot,'/Data')
+}
+OrigDataDir = paste0(DataDir,'/BLSOrig')
+dir.create(OrigDataDir,recursive=T,showWarnings=F)
 
 library(rvest) # XML/HTML handling
 
-FileListRaw <- html(BLSDataURL)
-FileList <- (FileListRaw %>% html_nodes('a') %>% html_text())
+FileListRaw = html(BLSDataURL)
+FileList = (FileListRaw %>% html_nodes('a') %>% html_text())
 
-Notes <- c()
-fileGet <- function() { 
-  cat("Starting file downloading...\n")
-  count.d <- 0
-  count.e <- 0
-  # [1] is [To Parent Directory] 
-  for(FileName in FileList[2:length(FileList)]) {
-    FilePath <- paste(OrigDataDir, FileName, sep='/')
-    if (file.exists(FilePath)==T) {
-      msg <- paste0(FileName, ' exists')
-      print(msg)
-      Notes <- c(Notes, msg)
-      count.e <- count.e + 1
-    } else {
-      FileURL <- paste(BLSDataURL, FileName, sep='/')
-      msg <- paste0('Downloading: ', FilePath, ' from: ', FileURL)
-      print(msg)
-      Notes <- c(Notes, msg)
-      download.file(FileURL, FilePath, mode='wb')
-      count.d <- count.d + 1
-    }
-  } 
-  cat("\nFinished downloading process.\n")
-  cat(paste(count.e, "out of 25 files present in directory.\n"))
-  cat(paste("(Downloaded", count.d, "files.)", sep=" "))
-  Notes <- Notes
-}
+FileGet = function()
+{
+    Notes = c()
+    cat("Starting file downloading...\n")
+    count.d <- 0
+    count.e <- 0
+    for(FileName in FileList[2:length(FileList)]) # [1] is [To Parent Directory]
+    {
+        FilePath = paste(OrigDataDir, FileName, sep='/')
+        if (file.exists(FilePath))
+        {
+            Notes=c(Notes,paste0('Skipping ', FilePath))
+            msg <- paste0(FileName, ' exists')
+            print(msg)
+            count.e <- count.e + 1
+            next
+        }
+        FileURL = paste(BLSDataURL, FileName, sep='/')
+        Notes=c(Notes,paste0('Downloading ', FilePath, ' from ', FileURL))
+        msg <- paste0('Downloading: ', FilePath, ' from: ', FileURL)
+        print(msg)
+        download.file(FileURL, FilePath, mode='wb')
+        count.d <- count.d + 1
+    } # for each file
+    cat("\nFinished downloading process.\n")
+    cat(paste(count.e, "out of 25 files present in directory.\n"))
+    cat(paste("(Downloaded", count.d, "files.)", sep=" "))
+    print(Notes)
+} # FileGet
 
-fileGet()
-Notes
+FileGet()
