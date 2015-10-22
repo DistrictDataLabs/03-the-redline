3e21ad7d12a6f6863e10ab595631f9e0969c553c
Wat Hughes <wat.hughes@gmail.com>
2015-10-22 15:27:32 -0400

    Moved the best tab to the front to make it the default, followed by its doc. Also, minor doc edits.

--- ShinyEDA/app.R
+++ ShinyEDA/app.R
@@ -118,6 +118,85 @@ ui = fluidPage(
         type = "tabs",
         tabPanel
         (
+            'Injury Predictions by Industry',
+            sidebarLayout
+            (
+                sidebarPanel
+                (
+                    selectInput('datasetP', 'Choose a data.table:',
+                                choices = names(FNsP)),
+                    numericInput('yearsP','Number of years to predict:',3,min=1,max=9)
+                ),
+                mainPanel
+                (
+                    h3('Injuries (Series) by Year for the Selected Industry'),
+                    plotOutput('plot1P'),
+                    h3('Currently Selected:'),
+                    verbatimTextOutput('selTxtP'),
+                    hr(),
+                    shinyTree('treeP')
+                )
+            )
+        ), # tabPanel - P
+        tabPanel
+        (
+            'More Documentation',
+            titlePanel('Application Documentation and Background'),
+            br(),
+            'Each year, the United States Bureau of Labor Statistics (BLS) makes available',
+            'scores of GB of data on occupational injuries.',
+            'The data is available at',
+            tags$a(href='http://download.bls.gov/pub/time.series/cs',target='_blank','download.bls.gov/pub/time.series/cs'),
+            'and data documentation',
+            'is available in the file cs.txt at that location. Even more documentation is at',
+            tags$a(href='http://www.bls.gov/iif/oiics_manual_2010.pdf',target='_blank','www.bls.gov/iif/oiics_manual_2010.pdf'),
+            '. Briefly, injury series are recorded with many',
+            'dimensions such as year(s), age (range), and industry, to name a few.',
+            br(),br(),
+            'This application lets the user explore injuries by year for a chosen industry',
+            'or industry group. The application',
+            'displays a plot of the chosen data to help elucidate the trend in the chosen data.',
+            'To make the trend clear, the plot shows both the actual BLS data and a linear prediction',
+            'for the years following those available in the BLS data.',
+            'The actual BLS injury series counts are in black and the predicted injury series counts are in red.',
+            br(),br(),
+            'There are two user controls. The first is a text entry box that lets the user choose how',
+            'many years forward to predict.',
+            'The supported number of years to predict is between 1 and 9 inclusive.',
+            'The second lets the user choose a focus industry',
+            'or industry group by walking',
+            'a hierarchy that starts with all injured workers and gets more specific as the user',
+            tags$i('walks'),'towards',
+            'the leaves of the industry hierarchy tree.',
+            br(),br(),
+            'The industry hierarchy tree can be opened or closed by clicking on the triangle to left of each industry (category).',
+            'The user indicates a selection for plotting by clicking on the text of the industry (group) itself.',
+            'To provide adequate performance to the user, the depth of the industry hierarchy has been',
+            'limited and the summary counts have been precalculated by year and industry.',
+            br(),br(),
+            'When walking the tree, the user can click a white triangle to open its',
+            'respective node and view deeper hierarchy levels. When',
+            'a node is open, click the black triangle to close it.',
+            'Nodes without triangles have no deeper hierarchy levels.',
+            'These are leaf nodes and may correspond to an industry or a BLS grouping of',
+            'of industries. The non leaf nodes always correspond to industry groups.',
+            br(),br(),
+            '(Left) click on the', tags$b('Injury Predictions by Industry'),'tab in the upper left to return to the application.',
+            br(),br(),
+            'The source code for this application is available for review at the',
+            tags$a(href='https://github.com/WatHughes/03-the-redline',
+                   target='_blank','GitHub Private Repo'),
+            'for this application.',
+            br(),br(),
+            'To calculate the predictions, first the selected data points (specified by',
+            'the tree control)',
+            "are fed to R's lm()",
+            'function. The resulting model is then fed to predict() along with the list of years',
+            'that the user specified (indirectly, with the text input control).',
+            br(),br()
+        ), # tabPanel - Doc
+        tabPanel
+        (
             'BLS Datafiles',
             sidebarLayout
             (
@@ -178,7 +257,7 @@ ui = fluidPage(
                     shinyTree('treeT')
                 )
             )
-        ),
+        ), # tabPanel - T
         tabPanel
         (
             'Sample Tree Control for Depth Limited BLS Code Trees',
@@ -199,85 +278,7 @@ ui = fluidPage(
                     shinyTree('treeS')
                 )
             )
-        ),
-        tabPanel
-        (
-            'Injury Predictions by Industry',
-            sidebarLayout
-            (
-                sidebarPanel
-                (
-                    selectInput('datasetP', 'Choose a data.table:',
-                                choices = names(FNsP)),
-                    numericInput('yearsP','Number of years to predict:',3,min=1,max=9)
-                ),
-                mainPanel
-                (
-                    h3('Injuries (Series) by Year for the Selected Industry'),
-                    plotOutput('plot1P'),
-                    h3('Currently Selected:'),
-                    verbatimTextOutput('selTxtP'),
-                    hr(),
-                    shinyTree('treeP')
-                )
-            )
-        ),
-        tabPanel
-        (
-            'More Documentation',
-            titlePanel('Application Documentation and Background'),
-            br(),
-            'Each year, the United States Bureau of Labor Statistics (BLS) makes available',
-            'scores of GB of data on occupational injuries.',
-            'The data is available at',
-            tags$a(href='http://download.bls.gov/pub/time.series/cs',target='_blank','download.bls.gov/pub/time.series/cs'),
-            'and data documentation',
-            'is available in the file cs.txt at that location. Even more documentation is at',
-            tags$a(href='http://www.bls.gov/iif/oiics_manual_2010.pdf',target='_blank','www.bls.gov/iif/oiics_manual_2010.pdf'),
-            '. Briefly, injury series are recorded with many',
-            'dimensions such as year(s), age (range), and industry, to name a few.',
-            br(),br(),
-            'This application lets the user explore injuries by year for a chosen industry',
-            'or industry group. The application',
-            'displays a plot of the chosen data to help elucidate the trend in the chosen data.',
-            'To make the trend clear, the plot shows both the actual BLS data and a linear prediction',
-            'for the years following those available in the BLS data.',
-            'The actual BLS injury series counts are in black and the predicted injury series counts are in red.',
-            br(),br(),
-            'There are two user controls. The first is a text entry box that lets the user choose how',
-            'many years forward to predict.',
-            'The supported number of years to predict is between 1 and 9 inclusive.',
-            'The second lets the user choose a focus industry',
-            'or industry group by walking',
-            'a hierarchy that starts with all injured workers and gets more specific as the user',
-            tags$i('walks'),'towards',
-            'the leaves of the industry hierarchy tree.',
-            br(),br(),
-            'The industry hierarchy tree can be opened or closed by clicking on the triangle to left of each industry (category).',
-            'The user indicates a selection for plotting by clicking on the text of the industry (group) itself.',
-            'To provide adequate performance to the user, the depth of the industry hierarchy has been',
-            'limited and the summary counts have been precalculated by year and industry.',
-            br(),br(),
-            'When walking the tree, the user can click a white triangle to open its',
-            'respective node and view deeper hierarchy levels. When',
-            'a node is open, click the black triangle to close it.',
-            'Nodes without triangles have no deeper hierarchy levels.',
-            'These are leaf nodes and may correspond to an industry or a BLS grouping of',
-            'of industries. The non leaf nodes always correspond to industry groups.',
-            br(),br(),
-            '(Left) click on the', tags$b('Injury Predictions by Industry'),'tab in the upper left to return to the application.',
-            br(),br(),
-            'The source code for this application is available for review at that',
-            tags$a(href='https://github.com/WatHughes/03-the-redline',target='_blank','GitHub Repo'),
-            'for this application.',
-            br(),br(),
-            'To calculate the predictions, first the selected data points (specified by',
-            'the tree control)',
-            "are fed to R's lm()",
-            'function. The resulting model is then fed to predict() along with the list of years',
-            'that the user specified (indirectly, with the text input control).',
-            br(),br()
-        ) # tabPanel
+        ) # tabPanel - S
     ) # tabsetPanel
 ) # ui
 
